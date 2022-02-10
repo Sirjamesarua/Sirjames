35556bedcfdee3f4bbf3fc9173b8e9fde95993f6
sirjamesarua <dev.sirjames@gmail.com>
2022-02-09 20:35:10 -0800

    updating db config

--- config/database.php
+++ config/database.php
@@ -16,7 +16,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -64,11 +65,25 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
+       /* 'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
             'host' => isset($DATABASE_URL['host']) ? $DATABASE_URL['host'] : null,
             'port' =>  isset($DATABASE_URL['port']) ? $DATABASE_URL['port'] : null,
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL['user'],
+            'password' => $DATABASE_URL['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],*/
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => $DATABASE_URL['host'],
+            'port' =>  $DATABASE_URL['port'],
             'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL['user'],
             'password' => $DATABASE_URL['pass'],
