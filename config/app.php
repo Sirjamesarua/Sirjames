71085fde9da68172af2f769f93dcd2aa6876c36f
sirjamesarua <dev.sirjames@gmail.com>
2022-02-15 04:54:56 -0800

    setup

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'sirjames'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://sirjames.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
